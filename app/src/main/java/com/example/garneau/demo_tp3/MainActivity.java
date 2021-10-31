8c70110b5f808f7a1a76391a8a72394b6f595898
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-10-31 18:44:28 -0400

    31/10

--- app/src/main/java/com/example/garneau/demo_tp3/MainActivity.java
+++ app/src/main/java/com/example/garneau/demo_tp3/MainActivity.java
@@ -3,10 +3,18 @@ package com.example.garneau.demo_tp3;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+//import android.widget.Toolbar;
+//import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.example.garneau.demo_tp3.databinding.AppBarMainBinding;
+import com.example.garneau.demo_tp3.databinding.FragmentHomeBinding;
+import com.example.garneau.demo_tp3.model.Location;
+import com.example.garneau.demo_tp3.ui.home.HomeFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.lifecycle.LiveData;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,25 +24,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.garneau.demo_tp3.databinding.ActivityMainBinding;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     private NavController navController;
+    private Toolbar app_bar_main;
+    private HomeFragment homeFragment;
+    private FragmentHomeBinding fragmentHomeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // todo : bonne instanciation du binding
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // todo : ActionBar --> toolBar
+        app_bar_main = binding.toolbar.tbar;
+        app_bar_main.setSubtitle("Cours mobile");
+        app_bar_main.setTitle("TP 3");
+        app_bar_main.setLogo(R.drawable.ic_launcher_foreground);
+//        app_bar_main = (Toolbar)findViewById(R.id.tbar);
+//        setSupportActionBar(app_bar_main);
+//        getSupportActionBar().setSubtitle("Cours mobile");
+//        getSupportActionBar().setTitle("TP 3");
+//        getSupportActionBar().setLogo(R.drawable.ic_launcher_foreground);
 
 
         // todo : instanciation des éléments du layout
 
+        fragmentHomeBinding = FragmentHomeBinding.inflate(getLayoutInflater(),MainActivity,flase);
+
         // todo : bonne instanciation du AppBarConfiguration.
+
+
 
         // todo : bonne déclaration et instanciation du NavController
 
